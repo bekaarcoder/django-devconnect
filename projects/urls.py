6bfb3179ba89bfd59258f00d95439afa5073167b
Shashank <s_shashank@outlook.com>
2021-07-19 21:10:16 +0530

    added CRUD functionality

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import projects, project
+from .views import (
+    projects,
+    project,
+    createProject,
+    updateProject,
+    deleteProject,
+)
 
 
 urlpatterns = [
     path("", projects, name="projects"),
     path("project/<str:pk>/", project, name="single-project"),
+    path("create-project/", createProject, name="create-project"),
+    path("update-project/<str:pk>/", updateProject, name="update-project"),
+    path("delete-project/<str:pk>/", deleteProject, name="delete-project"),
 ]
