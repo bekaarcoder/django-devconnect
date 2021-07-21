2683306a1b499689d10356aa701359b9ec52cd75
Shashank <s_shashank@outlook.com>
2021-07-21 21:06:18 +0530

    added templates for single-project and project form

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -13,3 +14,33 @@ class ProjectForm(ModelForm):
             "tags",
             "featured_image",
         ]
+        widgets = {"tags": forms.CheckboxSelectMultiple()}
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Add Title"}
+        )
+
+        self.fields["description"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "placeholder": "Write a description for your project...",
+                "rows": "3",
+            }
+        )
+
+        self.fields["demo_link"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Add a demo link"}
+        )
+
+        self.fields["source_link"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Add a source link"}
+        )
+
+        self.fields["featured_image"].widget.attrs.update(
+            {"class": "form-control"}
+        )
+
+        self.fields["tags"].widget.attrs.update({"class": "form-tags"})
