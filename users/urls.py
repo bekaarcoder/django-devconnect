f7c60748df8cc1a24af2835d8e3d9ad2737c25f2
Shashank <s_shashank@outlook.com>
2021-07-27 22:40:27 +0530

    added profile lists and profile page with templates

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import profiles
+from .views import profiles, user_profile
 
 
 urlpatterns = [
     path("", profiles, name="profiles"),
+    path("profile/<str:pk>/", user_profile, name="user-profile"),
 ]
