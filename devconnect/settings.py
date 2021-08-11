612b4d8425462e519ad45816a4a42229cff02b2d
Shashank <s_shashank@outlook.com>
2021-08-11 21:32:48 +0530

    resetting password

--- devconnect/settings.py
+++ devconnect/settings.py
@@ -43,7 +43,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "projects.apps.ProjectsConfig",
     "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
