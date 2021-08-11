f45c1f7eda03fb6108e3a09a2417f63a3204e60a
Shashank <s_shashank@outlook.com>
2021-08-11 22:50:21 +0530

    django rest framework and serializer

---
+++ api/views.py
@@ -0,0 +1,31 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
+
+
+@api_view(["GET"])
+def get_routes(request):
+    routes = [
+        {"GET": "api/projects"},
+        {"GET": "api/projects/id"},
+        {"POST": "api/projects/id/vote"},
+        {"POST": "api/users/token"},
+        {"POST": "api/users/token/refresh"},
+    ]
+    return Response(routes)
+
+
+@api_view(["GET"])
+def get_projects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_project(request, pk):
+    projects = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(projects, many=False)
+    return Response(serializer.data)
--- devconnect/settings.py
+++ devconnect/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "crispy_forms",
     "crispy_bootstrap5",
+    "rest_framework",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
